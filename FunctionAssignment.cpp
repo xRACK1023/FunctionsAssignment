9b5b4b0aed7d224c0a7b6ad34844b81e0d0df57c
xrack1023 <jkparrett10@gmail.com>
2023-10-08 20:37:20 -0600

    initial start

---
+++ FunctionAssignment.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+void printNumsBackward(int start, int end){
+	if (start);
+
+}
+float feet2meeters(float f_lengh);
+
+
+float meters2feet(float m_length);
+
+int factorial(int n);
+
+float average(int a, int b, int c);
+
+int main() {
+
+	int start, end, a, b, c, n;
+	float f_length;
+
+	printf("Enter a start and end number (# #): ");
+		scanf("%i %i", &start, &end);
+
+	printf("Enter feet distance: ");
+		scanf("%f", &f_length);
+
+	printf("Enter a number to find factorial: ");
+		scanf("%i", &n);
+
+	printf("Enter 3 numbers to average: ");
+		scanf("%i %i %i", &a, &b, &c);
+	return 0;
+}
